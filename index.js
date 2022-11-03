c467190b7578d18f9d154145dfc707156e72fd1d
BoJlkogaB <bojlkogab.03@gmail.com>
2022-11-03 19:39:15 +0300

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+import dotenv from 'dotenv'
+import TelegramBot from 'node-telegram-bot-api'
+import express from 'express'
+import cors from 'cors'
+
+dotenv.config();
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+const app = express();
+const clientUrl = process.env.CLIENT_URL
+const adminId = process.env.TELEGRAM_ADMIN_ID
+const managerIds = process.env.TELEGRAM_MANAGERS_ID
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+
+  await bot.sendMessage(chatId, '*Давайте начнем*\n\nПожалуйста, нажмите на кнопку ниже, чтобы заказать разработку', {
+    parse_mode: 'Markdown',
+    reply_markup: {
+      inline_keyboard: [
+        [{text: 'Сделать заказ', web_app: {url: clientUrl}}]
+      ]
+    }
+  })
+});
+
+app.post('/order', async (req, res) => {
+  const {queryId, order, userName, userId} = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      id: queryId,
+      type: 'article',
+      title: 'Услуга заказана',
+      input_message_content: {
+        message_text: ` Вы успешно заказали услугу: ${order}. Мы скоро свяжемся с Вами!`
+      }
+    })
+
+    await bot.sendMessage(adminId, `Заказ от пользователя: ${userName}.\nУслуга: ${order}. ID пользователя:  ${userId}`)
+
+    managerIds.map(async (managerId) => {
+      await bot.sendMessage(managerId, `Заказ от пользователя: ${userName}.\nУслуга: ${order}.`)
+    })
+
+    return res.status(200).json({});
+  } catch (e) {
+    return res.status(500).json({})
+  }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
